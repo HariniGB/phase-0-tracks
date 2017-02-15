43cc0aa2da1b89a18e0d00a6c2d9caec1d5826e6
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-02-15 12:58:20 -0800

    Add conditional logic practice statemnets in the file.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,13 +1,30 @@
+wolves_like_sunshine = true
+wolves_like_garlic = true
+vampires_like_sunshine = false
+vampires_like_garlic = false
+puts wolves_like_garlic && wolves_like_sunshine
+puts wolves_like_garlic || vampires_like_garlic
+puts wolves_like_garlic && vampires_like_garlic
+puts wolves_like_garlic && (vampires_like_sunshine || vampires_like_garlic)
+puts (wolves_like_garlic && vampires_like_garlic) || wolves_like_sunshine
+puts vampires_like_garlic
+puts !vampires_like_garlic
+puts !(wolves_like_sunshine && wolves_like_garlic)
+
 puts "Welcome to the interview\nplease be seated!"
 puts "What is your name?"
 name = gets.chomp
 puts "How old are you?"
 age = gets.chomp.to_i
 puts "What year were you born?"
-year = gets.chomp
-current_year = Date.today.year 
+year = gets.chomp.to_i
+current_year = Time.new.year 
 birth_year = current_year - year
     if age != birth_year 
+    	puts "The age is not equal to the actual birth age."
+    else
+    	puts "Yes he/she is right."
+    end
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 garlic_taste = gets.chomp
 if garlic_taste.upcase == "N" || garlic_taste.upcase == "NO"  || garlic_taste == ""
