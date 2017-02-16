0d0cd37db51ba5928fb016c0c3febd140f3afe30
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-02-15 17:26:27 -0800

    Add plot twist sentence in the bottom of the file and removed italian test conditions.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,4 @@
+def interview_test
 puts "How many employees are to be interviwed today?(numbers)"
 total_count = gets.chomp.to_i
 for count in 1..total_count
@@ -18,13 +19,6 @@ if garlic_taste.upcase == "N" || garlic_taste.upcase == "NO" || garlic_taste == 
 else
   garlic_taste = true
 end
-puts "There is a famous Italian restaurant next to our office. It will be great to eat lunch with you.\nDo you like Italian foods?(y/n)"
-italian_taste = gets.chomp
-if italian_taste.upcase == "N" || italian_taste.upcase == "NO" || italian_taste == ""
-  italian_taste = false
-else
-  italian_taste = true
-end
 puts "Would you like to enroll in the company's health insurance?(y/n)"
 health_insurance = gets.chomp
 if health_insurance.upcase == "Y" || health_insurance.upcase == "YES" || health_insurance.upcase == "WAIVES"
@@ -32,6 +26,13 @@ if health_insurance.upcase == "Y" || health_insurance.upcase == "YES" || health_
 else
   health_insurance = false
 end
+allergy_test(count, person_name, age, year, correct_age, garlic_taste, health_insurance)
+end
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+end
+
+
+def allergy_test(count, person_name, age, year, correct_age, garlic_taste, health_insurance)
 puts "Do you have any allergies? (y/n)"
 allergies = gets.chomp
 if (allergies.upcase == "Y" || allergies.upcase == "YES")
@@ -40,29 +41,40 @@ if (allergies.upcase == "Y" || allergies.upcase == "YES")
 		puts "What allergy do you have? (if nothing enter - Done)"
 		allergy_type = gets.chomp 
 	end
-	if allergy_type.upcase == "SUNSHINE"
-		puts "Thanks for coming!"
-		puts "The employee#{count} details:\nThe name of the employee is #{person_name}, #{age} years old. Was born in #{year}."
-        puts "This employee #{if garlic_taste; "likes "; else " hates "; end} garlic bread."
-        puts "This employee #{if health_insurance ; "wants to enroll to "; else "waives "; end} the company's health insurance."
-        puts "According to the interview this employee : Probably a vampire."  
-        next  
-    end
+	if allergy_type.upcase == "SUNSHINE" 
+		result = "Probably a vampire"
+		display_details(count, person_name, age, year, garlic_taste, health_insurance,result)
+    else
+    vampire_test(count, person_name, age, year, correct_age, garlic_taste, health_insurance, result)
+  end
+else
+ vampire_test(count, person_name, age, year, correct_age, garlic_taste, health_insurance, result)
 end
+end
+
+
+def vampire_test(count, person_name, age, year,correct_age, garlic_taste, health_insurance, result)
 if person_name.upcase == "DRAKE CULA" || person_name.upcase == "TU FANG"
   result = "Definitely a vampire"
-elsif correct_age == true && (garlic_taste == true || italian_taste == true) && health_insurance == true
+elsif correct_age == true && garlic_taste == true  && health_insurance == true
   result = "Probably not a vampire."
-elsif correct_age == false && ((garlic_taste == false || italian_taste == false) || health_insurance == false)
-  result = "Probably a vampire."
-elsif correct_age == false && (garlic_taste == false || italian_taste == false) && health_insurance == false
+elsif correct_age == false && garlic_taste == false && health_insurance == false
   result = "Almost certainly a vampire."
+elsif correct_age == false && garlic_taste == false || health_insurance == false
+  result = "Probably a vampire."
 else
   result = "Results inconclusive."
 end
+display_details(count, person_name, age, year, garlic_taste, health_insurance, result) 
+end
+
+
+def display_details(count, person_name, age, year, garlic_taste, health_insurance,result)
 puts "Thanks for coming!"
 puts "The employee#{count} details:\nThe name of the employee is #{person_name}, #{age} years old. Was born in #{year}."
 puts "This employee #{if garlic_taste; "likes "; else " hates "; end} garlic bread."
 puts "This employee #{if health_insurance ; "wants to enroll to "; else "waives "; end} the company's health insurance."
 puts "According to the interview this employee : #{result}"
 end
+
+puts interview_test
