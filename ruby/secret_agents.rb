6743920c3b0fdf3b0232f69377dff6a8d67b8d3b
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-02-17 13:17:48 -0800

    Add Pseudocode for the program and comment my explanations.

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,3 +1,14 @@
+#PSEUDOCODE
+#Create a function name and get a string as an argument.
+#Create a variable (ex:i) and assign its value to zero.
+#Using this variable i, iterate through the string using string[i] indexes.
+#To encrypt to the alphanet in the next index, use next method with bang operator.
+#To decrypt to the previous alphabet in the aphabets string, find the index value of the current letter and minus 1 from that index.
+#Replace the given letter with the letter in its previous index value. 
+#Call nested method and check the method's return values. 
+#Add a driver code to ask the user for their interest and the word to be encrypted/decrypted. 
+
+#METHOD DECLARATION
 def encrypt(word)
 	i = 0
 	while i < word.length
@@ -35,8 +46,11 @@ end
 #puts "Decrypt and Encrypt:  Nested Method Call"
 #puts "The word is : swordfish"
 #puts decrypt(encrypt("swordfish"))
-# This is not a nested method, this is a method being called on another method. Both functions are separate of each other which allows them to be used on one another.
+# This is a nested method. It is used to call encrypt method as an argument to decrypt method. 
+# Both functions are separate of each other which allows them to be used on one another.
+#As it is not a nested function definition, where a functio nis defined inside another function. This works well. 
 
+#DRIVER CODE. 
 puts 'Would you like to encrypt or decrypt a password?'
 answer = gets.chomp
 puts 'What is your password?'
