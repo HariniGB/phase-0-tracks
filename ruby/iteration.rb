d3d924103b095c31d2a93b0b034f583b3b3063f0
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-02-20 10:43:11 -0800

    Add a hash and use .each in the file.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,8 +1,22 @@
-def names 
-	name2 = "Harini"
-	puts "Pairing session 5.3 Iterations"
-	yield("Natalie",name2)
-	puts "Great! Learned .each , .times and .map methods"
-end 
+#def names 
+#	name2 = "Harini"
+#	puts "Pairing session 5.3 Iterations"
+#	yield("Natalie",name2)
+#	puts "Great! Learned .each , .times and .map methods"
+#end 
 
-names {|x, name2| puts "#{x} and #{name2}" }
+#names {|x, name2| puts "#{x} and #{name2}" }
+
+fruits = { "Apricots" => "Vitamin A",
+"Apple" => "Vitamin B1",
+"Banana" => "Vitamin C",
+"Blackberries" => "Vitamin B2",
+"Kiwi" => "Vitamin B6"
+}
+
+fruits.each {|fruit, vitamin| puts "In this #{fruit} we have #{vitamin}"}
+
+fruits.each do|fruit, vitamin|
+	puts "This is using do and end"
+	puts "In this #{fruit} we have #{vitamin}"
+end
