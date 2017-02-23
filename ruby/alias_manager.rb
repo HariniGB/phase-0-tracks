0e1e794f0c9a838b8769e339b1a43824a2b0b89d
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-02-22 17:03:10 -0800

    Add a data structure to list all the names and fake names converted so far in the file.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,29 +3,38 @@
 #Pseudocode 
 #Create a method that take the spy's real name (string) and returns the fake name.
 #Swapping the first and last name
-#Changing all vowels to next vowels and all consonants to their next letter.
+#Changing all vowels to next vowel letter and all consonants to their next consonant letter.
+#Use methods like next_vowel helps debugging
+#Create user interface and get the name else type quit.
+#Store the names in a data structure (like array) and display when the user quits.
+#Display the names list in a sentence "Vussit Gimodoe is actually Felicia Torres
 
-#release 1
+#release 2
 def alias_name()
 #user Interface
 	puts "Fake name converter!!!"
 	puts "----------------------"
 	name = ""
-    until name == "quit" do
-    	puts "Enter the spy's real name: (enter 'quit' to end)"
-    	name = gets.chomp
-    	if name != "quit"
-    		fake_name = []
-		    swap_names(name).each_char do |letter|
-		    	fake_name << next_vowel(letter)
-	      end
-	      puts "The fake name is: "
-	      puts capital_name(fake_name.join(''))
-	    else  
-		    puts "You have quit successfully!"
-		    exit 
-		  end
-	  end 
+	name_list = []
+  until name == "quit" do
+    puts "Enter the spy's real name: (enter 'quit' to end)"
+    name = gets.chomp
+    if name == "quit"
+    	puts "You have quit successfully!"
+		  puts name_list
+		  exit
+	  else
+	    fake_name = []
+		  swap_names(name).each_char do |letter|
+		   	fake_name << next_vowel(letter)
+	    end
+	    fake = capital_name(fake_name.join(''))
+	    puts "The fake name is: "
+	    puts fake
+	    name_history = fake + " is actually "+ name
+	    name_list << name_history
+		end
+	end 
 end
 #swapped the first name and the last name 
 def swap_names(name)
@@ -33,16 +42,20 @@ def swap_names(name)
 	swap_name = new_name.reverse.join(" ")
 end
 #converted all vowels to next vowels and consonants to next letter.
-def next_vowel(letter)
+def next_vowel(char)
+	letter = char.downcase
 	vowels = "aeiou"
+	consonant = "bcdfghjklmnpqrstvwxyz"
 	if vowels.index(letter) != nil && letter == vowels[-1]
 		letter = vowels[0]
 	elsif vowels.index(letter) != nil  
-		letter = vowels[vowels.index(letter) + 1]
+		letter = vowels[vowels.index(letter)+1]
 	elsif letter == " "
 		letter = " "
-	else 
-	  letter = letter.next
+	elsif consonant.index(letter) != nil && letter == consonant[-1]
+	  letter = consonant[0]
+	elsif consonant.index(letter) != nil
+		letter = consonant[consonant.index(letter)+1]
 	end   
 	return letter
 end
