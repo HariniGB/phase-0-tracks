a4d85faba9252d2a8611e1cc2d8c9dca15c8855a
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-02-22 16:37:17 -0800

    Add user interface to get the name and return a fake name else quit.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -5,27 +5,53 @@
 #Swapping the first and last name
 #Changing all vowels to next vowels and all consonants to their next letter.
 
-#release 0
-def alias_name(name)
-	vowels = "aeiou"
-	fake_name = []
-	new_name = name.split(' ')
-	swap_name = new_name[1] + " " + new_name[0]
-	#swapped the first name and the lst name 
-	swap_name.each_char do |c|
-		if vowels.index(c) != nil && c == vowels[-1]
-			c = vowels[0]
-		elsif vowels.index(c) != nil  
-			c = vowels[vowels.index(c) + 1]
-		elsif c == " "
-			c = " "
-		else 
-			c = c.next
-		end
-		fake_name << c
-	end
-	#converted all vowels to next vowels and consonants to next letter. 
-	return fake_name.join('')
+#release 1
+def alias_name()
+#user Interface
+	puts "Fake name converter!!!"
+	puts "----------------------"
+	name = ""
+    until name == "quit" do
+    	puts "Enter the spy's real name: (enter 'quit' to end)"
+    	name = gets.chomp
+    	if name != "quit"
+    		fake_name = []
+		    swap_names(name).each_char do |letter|
+		    	fake_name << next_vowel(letter)
+	      end
+	      puts "The fake name is: "
+	      puts capital_name(fake_name.join(''))
+	    else  
+		    puts "You have quit successfully!"
+		    exit 
+		  end
+	  end 
 end
-puts "The real name is Felicia Torres"
-puts alias_name("Felicia Torres")
+#swapped the first name and the last name 
+def swap_names(name)
+	new_name = name.split(" ")
+	swap_name = new_name.reverse.join(" ")
+end
+#converted all vowels to next vowels and consonants to next letter.
+def next_vowel(letter)
+	vowels = "aeiou"
+	if vowels.index(letter) != nil && letter == vowels[-1]
+		letter = vowels[0]
+	elsif vowels.index(letter) != nil  
+		letter = vowels[vowels.index(letter) + 1]
+	elsif letter == " "
+		letter = " "
+	else 
+	  letter = letter.next
+	end   
+	return letter
+end
+#Capitalize all the first letters in the name. 
+def capital_name(name)
+	capital = name.split(" ")
+	capital.each do |word|
+		word[0] = word[0].capitalize
+	end
+	name = capital.join(" ")
+end
+puts alias_name
