39f6828f64c95abe44c416e14f0a4f64df58a24a
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-02-27 16:53:10 -0800

    Add the pseudocode to the file.

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,37 @@
+# PSEUDOCODE
+# Create a new list and add,remove, update and pritn the list. 
+# Using Hash data strucutre to create the list.
+
+
+# DRIVER CODE
+
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza") 
+# steps: 
+  # Convert the string parameter into an array of words.
+  # We have to iterate through the array  and get the element.
+  # Store the element as a key in the hash.
+  # set default quantity  an integer with value 3. This is the default value of the keys in the hash.
+  # print the list to the console [can you use one of your other methods here?]
+# output: Hash 
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps:
+   #
+# output: modified hash
+
+# Method to remove an item from the list
+# input: list, item name.
+# steps:
+# output: modified hash 
+
+# Method to update the quantity of an item
+# input:list, item name, old_quantity and new_quantity
+# steps:
+# output: return list, item name with new_quantity 
+
+# Method to print a list and make it look pretty
+# input: list
+# steps:
+# output: print each element and its quantity in a sentence using .each
