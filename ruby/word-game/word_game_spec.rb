3f0935ecb9f01e3422f1f60c24376631bac8ec3a
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-03-03 11:12:48 -0800

    Add 5 rspec test statements to make a correct guess and dispaly the congrats message with word_status.

--- ruby/word-game/word_game_spec.rb
+++ ruby/word-game/word_game_spec.rb
@@ -1,5 +1,5 @@
 # The user1 will give the word 
-# User 2 will make the guesses till he find the right word.
+# User2 will make the guesses till he find the right word.
 # There must be a guess_limit based on the length of the word.
 # Use a guessing_letter method to check whether its right or wrong.
 # If its right, decrement the count of the guesses. If its wrong add the guess in a list.
@@ -16,9 +16,38 @@
 require_relative 'word_game'
 
 describe WordGuess do 
-    let(:game) { WordGuess.new(word) } 
+    let(:wordgame) { WordGuess.new("DevBootcamp") } 
+
+    it 'is initially empty' do
+    	expect(@guessed_word).to be_empty
+    end
 
     it "stores the word given on initialization" do
-    expect(game.target_word).to eq ["DevBootcamp"]
-  end
+    	expect(wordgame.target_word).to eq "DevBootcamp"
+    end
+
+    it "gives the total number of guesses" do
+    	expect(wordgame.guess_limit).to eq 11
+    end
+
+    it "Convert the word to dashes" do
+    	expect(wordgame.dash_word).to eq "-----------"
+    end
+
+    it "get the guessed word and stores it" do
+    	expect(wordgame.correct_word("a",9)).to eq "Congrats!!! Your guess is correct. You have 10 guesses. Try to find the word."
+    end
+
+    it "feedback on the current state of the word" do
+    	expect(wordgame.word_status).to eq "--------a--"
+    end
+
+    it "after a guess has made, display the hash with the guess data" do
+    	expect(@guessed_word).to eq "{9 => 'a'}"
+    end
+
+    it "get the guessed word and check for repeat" do
+    	expect(wordgame.repeat_word("b",9)).to eq "Don't repeat the same guess. You have 10 guesses. Try different guess."
+    end
+
 end
