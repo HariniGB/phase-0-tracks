90fa9d10e058c8999c681f9aa6664acaf78e692a
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-03-02 16:42:58 -0800

    Create a TodoList class and add the methods based on the error message from Rspec todo_list_spec.rb file.

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,24 @@
+class TodoList
+
+	# Failure/Error: let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
+    # ArgumentError:
+    # wrong number of arguments (1 for 0)
+	def initialize(items)
+		@items = items
+	end
+
+	# NoMethodError: undefined method `get_items' for #<TodoList:0x007f9dcd079230>
+	def get_items()
+		@items 
+	end
+
+	#  NoMethodError: undefined method `add_item' for #<TodoList:0x007ff6a1869d50>
+	def add_item(item)
+		@items << item
+	end
+
+	# NoMethodError: undefined method `delete_item' for #<TodoList:0x007fc32411b018>
+	def delete_item(item)
+		@items.delete(item)
+	end
+end
