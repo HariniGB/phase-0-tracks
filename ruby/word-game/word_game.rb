b074e01b9f48b96a0b9aedaeb366ecf4731fae2a
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-03-03 21:26:17 -0800

    Add the user interface and call the class instance methods in the word_game.rb

--- ruby/word-game/word_game.rb
+++ ruby/word-game/word_game.rb
@@ -27,7 +27,7 @@
 # USER 1: Enter the word.
 # DevBootcamp 
 # USER 2: Try your guess. You have 11  guesses.
-# The word is _ _ _ _ _ _ _ _ _ _ _ : 11 letters.
+# The word is _ _ _ _ _ _ _ _ _ _ _ 
 # Enter the letter postion (1 to 11) and guess the aplhabet.
 # Enter the position: 
 # 2
@@ -68,8 +68,12 @@ class WordGuess
 		@guesses = word.length
 		# Initialize a instance variable with empty hash.
 		@guessed_words = {}
+		# Initialize the game_end to boolean false 
 		@game_end = false
+		# Index is used to replace the dashes with the correct guess.
 		@index = 0
+		# Initialize the hidden word with dashes instead of alphabets.
+		@hidden_word = word.tr(word,"-")
 	end
 
 	def original_word
@@ -81,8 +85,7 @@ class WordGuess
 	end
 
 	# Method to print the word as dash to guess the letters.
-	def dash_word
-		@hidden_word = original_word.tr(original_word,"-")
+	def hide_word	
 		hidden_word
 	end
 
@@ -101,23 +104,22 @@ class WordGuess
 	# Method to get the guessed word from the user. 
 	def guessed_word(alphabet,position)
 		index = position-1
-		if original_word[index] == alphabet.upcase
-			dash_word[index] = alphabet.upcase
+		if guessed_words.keys.index(position) != nil && guessed_words[position].upcase == alphabet.upcase
+			puts "Don't repeat the same guess"
+		elsif original_word[index] == alphabet.upcase
+			hidden_word[index] = alphabet.upcase
 			guesses_history(alphabet,position)
-			guesses_left
-			hidden_word
+			puts "Congrats!!! Your guess is correct."
 		elsif original_word[index] == alphabet.downcase
-			dash_word[index] = alphabet.downcase
+			hidden_word[index] = alphabet.downcase
 			guesses_history(alphabet,position)
-			guesses_left
-			hidden_word
-		elsif guessed_words.keys.index(position) != nil && guessed_words[position] == alphabet
-			puts "Repeated guess"
+			puts "Congrats!!! Your guess is correct."
 		else
+			puts "Sorry! That's a wrong guess."
 			guesses_history(alphabet,position)
-			hidden_word
 		end
-		hidden_word
+		puts "You have only #{guesses_left} guesses left."
+		puts "The status of the word is: #{hidden_word}" 
 	end
 
 	# A method to end the game if the user has guessed all the letters.
@@ -132,24 +134,46 @@ class WordGuess
 	end
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+## USER INTERFACE
+# Guess the WORD
+puts "Welcome to the 'GUESS the WORD game!!!'"
+puts "-" * 38
+puts "Game procedure:"
+puts "There must be two players.\nOne player can enter a word to be guessed and the another player attempts to guess the word.."
+puts "***** RULES *****"
+puts "1. Guesses are limited, and the number of guesses available is related to the length of the word."
+puts "2. Repeated guesses do not count."
+puts "3. You will receive continual feedback on the current state of the word."
+puts "LET'S PLAY ....... GUESS the WORD!"
+puts "-" * 34
+puts "Player 1: Please enter a word"
+word = gets.chomp
+wordgame = WordGuess.new(word)
+puts "Player 2: You have #{wordgame.guesses} guesses"
+puts "The word is: #{wordgame.hidden_word}"
+until wordgame.guesses <= 0
+	#Initialize the local variables inside the loop to valid the until condition.
+	position = 0
+    alphabet = ""
+	# Check whether the position is a valid position number 
+	until position >= 1 && position <=  word.length
+		puts "Enter the letter's postion (1 to #{word.length}):"
+		position = gets.chomp.to_i
+		puts "Invalid position." if (position < 1 || position > word.length)
+	end
+	# Check whether the guess is an alphabet and not any other invalid data.
+	until alphabet.match(/^[[:alpha:]]$/)
+		puts "Enter your guess (alphanets):"
+		alphabet = gets.chomp
+		puts "Invalid alphabet." if !alphabet.match(/^[[:alpha:]]$/)
+	end
+	wordgame.guessed_word(alphabet,position)
+end
+puts "-" * 50 
+if  wordgame.game_end? == true
+	puts "Thanks for participation."
+	puts "The status of the word is: #{wordgame.hidden_word}"
+	puts "Better luck next time."
+elsif wordgame.game_end? == true && wordgame.hidden_word == word
+	puts "CONGRADULATIONS YOU WIN!!!!!!"
+end
