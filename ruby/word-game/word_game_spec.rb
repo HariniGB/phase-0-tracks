4c1216c340311ed81ed48d1a1313bbf2f2e77722
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-03-03 21:29:55 -0800

    Add complete test for the word 'DevBootcamp' with game_end expect result.

--- ruby/word-game/word_game_spec.rb
+++ ruby/word-game/word_game_spec.rb
@@ -30,11 +30,11 @@ describe WordGuess do
     end
 
     it "Convert the word to dashes" do
-    	expect(wordgame.dash_word).to eq "-----------"
+    	expect(wordgame.hide_word).to eq "-----------"
     end
 
     it "check whether the guesses word is a correct guess" do
-    	expect(wordgame.guessed_word("a",9))=="--------a--"
+    	expect(wordgame.guessed_word("a",9))=="The status of the word is: --------a--"
     end
 
     it "after a correct guess the limit decrements" do
@@ -42,7 +42,7 @@ describe WordGuess do
     end
 
     it "feedback on the current state of the word" do
-    	expect(wordgame.guessed_word("e",9))== "--------a--"
+    	expect(wordgame.guessed_word("e",9))== "The status of the word is: --------a--"
     end
 
     it "after a guess has made, store the guess in the hash" do
@@ -50,35 +50,42 @@ describe WordGuess do
     end
 
     it "get the guessed word and check for correct match" do
-        expect(wordgame.guessed_word("a",9))== "Repeated guess"
+        expect(wordgame.guessed_word("a",9))== "Don't repeat the same guess"
     end
 
     it "is the game over" do
         p wordgame.original_word
-        p wordgame.dash_word
+        p wordgame.hide_word
         p wordgame.guess_limit
         p wordgame.guessed_word("D",1)
+        p wordgame.guesses_history("D",1)
         p wordgame.guess_limit
         p wordgame.guessed_word("e",2)
+        p wordgame.guesses_history("e",2)
         p wordgame.guess_limit
         p wordgame.guessed_word("v",3)
+        p wordgame.guesses_history("v",3)
         p wordgame.guess_limit
         p wordgame.game_end?
-        p wordgame.guessed_word("B",4)
+        p wordgame.guessed_word("b",4)
         p wordgame.guessed_word("P",11)
-        p wordgame.guessed_word("o",6)
         p wordgame.guessed_word("o",6)
         p wordgame.guess_limit
         p wordgame.game_end?
         p wordgame.guessed_word("t",7)
-        p wordgame.guess_limit
         p wordgame.guessed_word("o",5)
         p wordgame.guessed_word("o",5)
         p wordgame.guess_limit
+        p wordgame.guesses_history("o",5)
         p wordgame.guessed_word("c",8)
         p wordgame.guessed_word("a",9)
+        p wordgame.guess_limit
         p wordgame.guessed_word("m",10)
-        expect(wordgame.game_end?).to eq false
+        p wordgame.guessed_word("t",7)
+        p wordgame.guess_limit
+        p wordgame.guesses_history("o",5)
+        p wordgame.game_end?
+        expect(wordgame.game_end?).to eq true
      end
 
 end
