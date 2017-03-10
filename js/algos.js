6b160ae1db29eb32c72d179db34081c8a6edec8a
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-03-10 12:55:41 -0800

    Add a function that takes an integer for length and returns an array of strings of the given length.

--- js/algos.js
+++ js/algos.js
@@ -37,6 +37,7 @@ function largest(array){
 }
 
 //DRIVER CODE
+console.log("-----------------------");
 console.log("Find the Longest Phrase");
 console.log("-----------------------");
 console.log(largest(["long phrase","longest phrase","longer phrase"]));
@@ -45,11 +46,10 @@ console.log(largest(["Joey's Double PIZZA","How're you dong?","We were on a brea
 // it would return "Judy, pick up the sock"
 console.log(largest(["Apple","Banana","Cranberry","Dates","Eat","Fruits","Grapes","Honey","ItaPalm","Juice","Kiwi","Lemon","Mango","Nimbu","Orange"]));
 // it would return "Cranberry" 
-console.log("-------------------------");
+console.log("-----------------------");
 
 /*
-Release 1
-Grouping functions 
+Release 1 : Find a Key-Value Match
 
 PSEUDOCODE
 -Create a function that takes two objects as parameters.
@@ -96,15 +96,19 @@ function same_values(obj1, obj2){
 		// Iterate through the obj2 using a variable 'j' as index.
 		for (var j = 0 ; j < obj2_values.length ; j++){
 
-			// Compares both the objects' values.
-			if (obj1_values[i] === obj2_values[j]){
+			// Compare both the object's keys. If the keys are same then check for values.
+			if (Object.keys(obj1)[i] === Object.keys(obj2)[j]){
 
-				// If they have one or more similar value, return true.
-				return true;
+				// Compares both the objects' values.
+				if (obj1_values[i] === obj2_values[j]){
+
+					// If both objects have same key and value, return true.
+					return true;
+				}
 			}
 		}
 	}
-	// If there is no same values, then return false.
+	// If there is no same key-value, then return false.
 	return false;
 }
 
@@ -117,10 +121,56 @@ obj2 = {name: "Tamir", age: 54};
 console.log(same_values(obj1, obj2));
 //would return true
 obj3 = {"Number": 5, "String": "Hello!"};
-obj4 = {"Boolean": true, "Float": 5.5};
+obj4 = {"Boolean": true, "Float": 5.5, "String": "Welcome"};
 console.log(same_values(obj3, obj4));
 //would return false
-console.log("------");
+console.log("------------------------");
+
+/*
+Release 2 : Generate Random Test Data.
+
+PSEUDOCODE
+-Create a function that takes an integer for length and returns an array of strings of given length.
+-We must have an array that stores various sizes of words from 1 to 10 letters.
+-Iterate through the array of words.
+-If the length of each word is equal to the input integer,
+-Push that word into a new array. Initialize this new array as empty in the beginning.
+-Return the new array with all the words of size  of the given integer.
+
+*/
+// Create a function with an integer parameter
+function words_of_length(size){
+
+	//Initialize an array of words of different sizes.
+	var all_words = ["A","X","Y","As","An","The","ICE","So","Cake","Bun","Bread","Apple","Mango","Hair","Band","Pineapple","Jackfruit","Skiing","Swimming","Dancing","Singing","Watching","Solutions","Problem","Encyclopedia","Generation","Population"];
+
+	//Initialize an empty array to store all the words og same size.
+	var sized_words = [];
+
+	//Iterate through the all_words array to check each element's length.
+	for(var i = 0 ; i < all_words.length ; i++ ){
+		//check if the word's length is equal to the given size integer.
+		if (all_words[i].length == size){
+
+			// Push the word inside the sized_word array if the condition is true.
+			sized_words.push(all_words[i]);
+		}
+	}
+	//return the sized_words array
+	return sized_words;
+}
+ 
+// DRIVER CODE
+console.log("Generate Random Test Data");
+console.log("-------------------------")
+console.log(words_of_length(3));
+//would return [ 'The', 'ICE', 'Bun' ]
+console.log(words_of_length(10));
+//would return [ 'Generation', 'Population' ]
+console.log(words_of_length(1));
+//would return [ 'A', 'X', 'Y' ]
+console.log("-------------------------")
+
 
 
 
