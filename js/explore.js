e1c99a4a5a91a85a4081c0463a0c9f3dd6946046
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-03-09 16:30:48 -0800

    Create a new javascript file

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+/*  Pseudocode 
+-Create a method which takes in a string and return the string in reverse
+-Store the input string in a variable.
+-Initialize an empty string as new variable.
+-Iterate through the string using a variable index in descensing order.
+-Create a variable for the index of the string and decrement it.
+-Add each letter to the new varaible(empty string).
+-return the new variable at the end.
+*/
