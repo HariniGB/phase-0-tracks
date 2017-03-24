ed0bc28f7645de6133dd4370c2195d5d2486342f
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-03-23 19:48:25 -0700

    Add a get/post method that inserts a new data inside subject table using form and redirect the file lin.

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -10,11 +10,17 @@ db.results_as_hash = true
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
+  @subjects = db.execute("SELECT * FROM subject")
   erb :home
 end
 
 get '/students/new' do
   erb :new_student
+end
+
+# Create a get http link to print the new_subject erb file.
+get '/subjects/new' do
+  erb :new_subject
 end
 
 # create new students via
@@ -23,5 +29,18 @@ post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+
+#create new subject via
+# a form
+get '/new_subject' do
+    db.execute("INSERT INTO subject (subject_name, student_id) VALUES (?,?)", [params['subject_name'], params['student_id'].to_i])
+  redirect '/'
+end 
+
+# post '/new_subject' do
+#   db.execute("INSERT INTO subject (subject_name, student_id) VALUES (?,?)", [params['subject_name'], params['student_id'].to_i])
+#   redirect '/'
+# end
 
 # add static resources
